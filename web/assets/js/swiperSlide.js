98e29e250ae1db5d7dd2a9bee9761a7caaaaf780
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2021-04-21 12:37:36 -0500

    Create  swiperSlide.js

---
+++ web/assets/js/swiperSlide.js
@@ -0,0 +1,15 @@
+//SWIPER ANIMATION ONE SLIDE CAROUSEL
+
+window.onload = () => {
+  var swiper = new Swiper('.swiper-container', {
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+};
