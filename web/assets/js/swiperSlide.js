a2ea3264fdaadd55bbb20a946d84039387a8a43f
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2021-04-21 12:59:42 -0500

    Update swiperSlide.js

--- web/assets/js/swiperSlide.js
+++ web/assets/js/swiperSlide.js
@@ -3,8 +3,8 @@
 window.onload = () => {
   var swiper = new Swiper('.swiper-container', {
     autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+      delay: 3500,
+      disableOnInteraction: true,
     },
     loop: true,
     navigation: {
